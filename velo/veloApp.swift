c6e099ff22bb6381e991bfbf1660a97b590c982d
juanramirezbl <najublanajubla@gmail.com>
2026-02-05 18:08:05 +0100

    feat: initial commit with correct root structure

---
+++ velo/veloApp.swift
@@ -0,0 +1,18 @@
+//
+//  veloApp.swift
+//  velo
+//
+//  Created by Juan Ramirez Blanco on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct veloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+        }
+    }
+}
---
+++ veloTests/veloTests.swift
@@ -0,0 +1,17 @@
+//
+//  veloTests.swift
+//  veloTests
+//
+//  Created by Juan Ramirez Blanco on 5/2/26.
+//
+
+import Testing
+@testable import velo
+
+struct veloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
